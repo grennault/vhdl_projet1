007c750b6d74be86de72a8c94d9ad52e6f4bb106
Vekkt <francois.burguet@epfl.ch>
2018-10-24 13:10:49 +0200

    Update PC.vhd

--- VHDL/PC.vhd
+++ VHDL/PC.vhd
@@ -17,19 +17,27 @@ entity PC is
 end PC;
 
 architecture synth of PC is
-	signal r : std_logic_vector(16 downto 0); 
+	signal r : std_logic_vector(15 downto 0); 
 begin
 	process (clk)	
 	begin
 		if (reset_n = '1') then 
 			addr <= (31 downto 0 => '0');
-			r <= (16 downto 0 => '0');
+			r <= (15 downto 0 => '0');
 		
 		elsif (rising_edge(clk)) then
-			if (en = '1') then 
-				r <= std_logic_vector('0' & to_unsigned(to_integer(unsigned(r))+4, 16));
+			if (en = '1') then
+				if (add_imm = '1') then
+					r <= std_logic_vector(unsigned(r) + unsigned(imm(15 downto 2) & "00"), 16));
+				elsif (sel_a = '1') then
+					r <= std_logic_vector(unsigned(r) + unsigned(a), 16));
+				else
+					r <= std_logic_vector(to_unsigned(to_integer(unsigned(r)) + 4, 16));
+				end if;
+
 				addr <= (31 downto 16 => '0') & r(15 downto 2) & "00";
 			end if;
+
 		end if;
 	end process;
 end synth;
